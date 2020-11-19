91c5243f2c9b41493f78ebfe5605db619332a741
yunhyun <cyh0107@gmail.com>
2020-11-19 16:06:15 +0900

    koa router 적용

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,34 @@
-// backend의 메인 파일. nodemon을 추가하면 수정하고 저장만해도 알아서 서버 재시작 
-
 const Koa = require('koa');
-// app 은 미들웨어 
+const Router = require('koa-router');
+
 const app = new Koa();
-// node src로 실행하면 서버가 가동되며, 브라우저로 localhost:4000 으로 접속하면 hello world 출력 
-// ctx 는 요청과 응답에 관한 정보(Context 줄임)
-// app.use(ctx => {
-//     ctx.body = 'hello world';
-// });
+const router = new Router();
 
-// app.use 는 ctx, next를 함께 받을 수 있음. next는 다음 app.use를 호출하라는 의미. Promise를 반환함 
-// 요청 주소에 authorized=1 이 있으면 다음 요청(app.use)을 호출하고 아니면 호출하지 않음. 
-// 호출이 제대로 되면 브라우저에 hello world 출력 
-// async 사용을 위해 async 추가 
-app.use(async (ctx, next) => {
-    console.log(ctx.url);
-    console.log(1);
-    
-    if(ctx.query.authorized !== '1') {
-        ctx.status = 401;
-        return;
-    }
-    // next().then(() => { //Promise 활용 1,2를 모두 출력하고 나서 END 가 출력됨 
-    //     console.log('END');
-    // });
-
-    // Promise 대신 async await 사용 
-    await next();
-    console.log('await END!!');
-
-});
-app.use((ctx, next) => {
-    console.log(2);
-    next();
+// 기본주소 요청 
+router.get('/', ctx => {
+    ctx.body = '홈';
 });
 
-app.use(ctx => {
-ctx.body = 'hello world';
+// about 주소 요청 
+router.get('/about', ctx => {
+    ctx.body = '소개';
 });
 
-// 터미널에 콘솔 내용 출력 
+// /about/aaa 요청 
+router.get('/about/:name?', ctx => {
+    const { name } = ctx.params;
+    ctx.body = name ? '${name} 소개' : '소개';
+});
+
+// /posts?id=10 요청 
+router.get('/posts', ctx => {
+    const { id } = ctx.query;
+    ctx.body = id ? '포스트 #${id}' : '포스트 아이디가 없습니다';
+});
+
+// app 인스턴스에 라우터 적용 
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(4000, () => {
-    console.log('4000 포트 연결');
-});
+    console.log('4000 포트 시작');
+})
