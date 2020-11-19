d1ea2056be42ce161fe1e54276b7ac963fa3fa66
yunhyun <cyh0107@gmail.com>
2020-11-19 16:34:07 +0900

    bodyparser 적용

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 // api 폴더에 생성한 index.js 사용 
 const api = require('./api');
@@ -9,6 +10,9 @@ const router = new Router();
 
 // 라우트 설정 
 router.use('/api', api.routes());
+
+// app에 라우터 적용전에 bodyParser 적용 
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용 
 app.use(router.routes()).use(router.allowedMethods());
