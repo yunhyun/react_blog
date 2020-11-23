68c28f36de2d189e2f0d278b929e8eacc0436f1b
yunhyun <cyh0107@gmail.com>
2020-11-23 14:23:48 +0900

    esm 적용

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -9,7 +9,7 @@ const posts = [
 ];
 
 // 글작성 
-exports.write = ctx => {
+export const write = ctx => {
     // rest api 의 요청 데이터는 ctx.request.body에 들어있음. 
     const { title, body } = ctx.request.body;
     postId += 1;
@@ -20,12 +20,12 @@ exports.write = ctx => {
 };
 
 // 글목록 
-exports.list = ctx => {
+export const list = ctx => {
     ctx.body = posts;
 };
 
 // 글조회 
-exports.read = ctx => {
+export const read = ctx => {
     const { id } = ctx.params;
     const post = posts.find(p => p.id.toString() === id);
     if(!post){
@@ -40,7 +40,7 @@ exports.read = ctx => {
 };
 
 // 글삭제 
-exports.remove = ctx => {
+export const remove = ctx => {
     const { id } = ctx.params;
     const index = posts.findIndex(p => p.id.toString() === id);  
     if(index === -1) {
@@ -55,7 +55,7 @@ exports.remove = ctx => {
 };
 
 // 글수정(전체 덮어쓰기) 
-exports.replace = ctx => {
+export const replace = ctx => {
     const { id } = ctx.params;
     const index = posts.findIndex(p => p.id.toString() === id);
     if(index === -1) {
@@ -74,7 +74,7 @@ exports.replace = ctx => {
 }
 
 // 글수정(일부 수정)
-exports.update = ctx => {
+export const update = ctx => {
     const { id } = ctx.params;
     const index = posts.findIndex(p => p.id.toString() === id);
     if(index === -1) {
