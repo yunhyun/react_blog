072a9c89046659f99d3eea01d2e23cf0a139f0a2
yunhyun <cyh0107@gmail.com>
2020-11-24 10:59:57 +0900

    id검증, 작성내용검증 추가

--- backend/src/api/posts/index.js
+++ backend/src/api/posts/index.js
@@ -7,9 +7,18 @@ const posts = new Router();
 // 각 주소가 호출되면 해당 함수 호출 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
+// id 검증 함수 추가 
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 // posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+
+// /api/posts/:id 용 라우터를 따로 만들어서 사용하는 방식 
+// const post = new Router();
+// post.get('/:id', postsCtrl.read);
+// post.delete('/:id', postsCtrl.remove);
+// post.patch('/:id', postsCtrl.update);
+
+// posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -1,7 +1,44 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
 
+/* 글조회, 글수정, 글삭제는 id를 받아와야 하는데 
+   id는 db에서 부여하는 것인데 양식이 복잡함(5fbb5b4159b11a1d800c20c2). 각 함수는 id가 맞지 않으면 404를 냄. 
+   따라서 id를 먼저 검증하고 글조회, 글수정, 글삭제 함수를 처리하도록 함 */
+
+// ObjectId 검증 함수 
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if(!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+    return next();
+};
+   
 // 글작성 
 export const write = async ctx => {
+    // 제목, 본문 등이 작성됐는지 검증 
+    const schema = Joi.object().keys({
+        // required()는 필수항목
+        title: Joi.string().required(),
+        body: Joi.string().required(),
+        // 배열에 대한 검증 
+        tags: Joi.array()
+            .items(Joi.string())
+            .required(),
+    });
+
+    // 검증 실패시 
+    const result = Joi.valid(ctx.request.body, schema);
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title, 
@@ -28,6 +65,7 @@ export const list = async ctx => {
     }
 };
 
+// 글조회 
 export const read = async ctx => {
     const { id } = ctx.params;
     try {
@@ -42,6 +80,7 @@ export const read = async ctx => {
     }
 };
 
+// 글삭제 
 export const remove = async ctx => {
     const { id } = ctx.params;
     try {
@@ -55,8 +94,20 @@ export const remove = async ctx => {
     }
 };
 
+// 글수정
 export const update = async ctx => {
     const { id } = ctx.params;
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),  
+    });
+    const result = Joi.valid(ctx.request.body, schema);
+    if (result.error) {
+        ctx.status = 400; // Bad Request
+        ctx.body = result.error;
+        return;
+    }
     try {
         /* findByIdAndUpdate함수는 매개변수가 3개 
             첫번째: id
