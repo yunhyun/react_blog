dea29d3bf4cc880dc4853a2338c5b82c1b34d8ea
yunhyun <cyh0107@gmail.com>
2020-11-24 11:28:41 +0900

    페이징, 내용길이제한 등 부가기능

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -21,23 +21,24 @@ export const checkObjectId = (ctx, next) => {
 // 글작성 
 export const write = async ctx => {
     // 제목, 본문 등이 작성됐는지 검증 
-    const schema = Joi.object().keys({
-        // required()는 필수항목
-        title: Joi.string().required(),
-        body: Joi.string().required(),
-        // 배열에 대한 검증 
-        tags: Joi.array()
-            .items(Joi.string())
-            .required(),
-    });
+    // const schema = Joi.object().keys({
+    //     // required()는 필수항목
+        
+    //     title: Joi.string().required(),
+    //     body: Joi.string().required(),
+    //     // 배열에 대한 검증 
+    //     tags: Joi.array()
+    //         .items(Joi.string())
+    //         .required(),
+    // });
 
-    // 검증 실패시 
-    const result = Joi.valid(ctx.request.body, schema);
-    if(result.error) {
-        ctx.status = 400;
-        ctx.body = result.error;
-        return;
-    }
+    // // 검증 실패시 
+    // const result = Joi.valid(ctx.request.body, schema);
+    // if(result.error) {
+    //     ctx.status = 400;
+    //     ctx.body = result.error;
+    //     return;
+    // }
 
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
@@ -56,10 +57,34 @@ export const write = async ctx => {
 
 // 글목록 
 export const list = async ctx => {
+    // 주소로 page 번호를 받아오고 없으면 1페이지로 
+    // 주소는 string이라 숫자로 변환 
+    const page = parseInt(ctx.query.page || '1', 10);
+    if(page < 1){
+        ctx.status = 400;
+        return;
+    }
+
     try {
         // find 호출 후 exec를 호출해야 서버에 쿼리를 요청함 
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        const posts = await Post.find()
+            .sort({ _id: -1 }) // 페이징을 위해 id 내림차순 정렬. 1이면 오름차순
+            .limit(3) // 한번에 3개씩
+            .skip((page-1) * 3) // 다음 페이지 3개 가져오기 
+            //.lean()  lean 함수를 쓰면 mongoDB에서 바로 JSON으로 가져옴 
+            .exec();
+        // 마지막 페이지 알려주기 
+        const postCount = await Post.countDocuments().exec();
+        ctx.set('Last-Page', Math.ceil(postCount / 3)); // http 헤더에 담음 
+        // body의 길이가 2자 이상이면 뒤에 ... 붙이기 
+        // mongoDB에서 조회한 내용을 json으로 변환 
+        ctx.body = posts
+            .map(post => post.toJSON())
+            .map(post => ({
+                ...post,
+                body:
+                    post.body.length < 2 ? post.body : `${post.body.slice(0, 2)}...`, // 싱글쿼터가 아니라 키보드 1왼쪽에 있는 키 
+            }));
     } catch (e)  {
         ctx.throw(500, e);
     }
@@ -97,17 +122,17 @@ export const remove = async ctx => {
 // 글수정
 export const update = async ctx => {
     const { id } = ctx.params;
-    const schema = Joi.object().keys({
-        title: Joi.string(),
-        body: Joi.string(),
-        tags: Joi.array().items(Joi.string()),  
-    });
-    const result = Joi.valid(ctx.request.body, schema);
-    if (result.error) {
-        ctx.status = 400; // Bad Request
-        ctx.body = result.error;
-        return;
-    }
+    // const schema = Joi.object().keys({
+    //     title: Joi.string(),
+    //     body: Joi.string(),
+    //     tags: Joi.array().items(Joi.string()),  
+    // });
+    // const result = Joi.valid(ctx.request.body, schema);
+    // if (result.error) {
+    //     ctx.status = 400; // Bad Request
+    //     ctx.body = result.error;
+    //     return;
+    // }
     try {
         /* findByIdAndUpdate함수는 매개변수가 3개 
             첫번째: id
